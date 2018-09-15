9f82714eaa2064c4e087a227f6c19dd549b578c1
Tine Jozelj <tine@tjo.space>
2018-09-15 13:32:52 +0200

    implemented splitscreen

--- Zelda?/player/player.gd
+++ Zelda?/player/player.gd
@@ -1,22 +1,33 @@
 extends KinematicBody2D
 
-const SPEED = 70
+enum keymaps {
+    wasd,
+    arrows,
+}
+
+export (int) var speed = 70
+export (keymaps) var keymap = keymaps.arrows
+
 
 var movedir = Vector2(0,0)
 
 func _physics_process(delta):
-	controls_loop()
-	movement_loop()
+    controls_loop()
+    movement_loop()
 
 func controls_loop():
-	var LEFT = Input.is_action_pressed("ui_left")
-	var RIGHT = Input.is_action_pressed("ui_right")
-	var UP = Input.is_action_pressed("ui_up")
-	var DOWN = Input.is_action_pressed("ui_down")
-	
-	movedir.x = -int(LEFT) + int(RIGHT)
-	movedir.y = -int(UP) + int(DOWN)
-	
+    var keymap_name = 'ui'
+    if keymap == keymaps.wasd:
+        keymap_name = 'wasd'
+
+    var LEFT = Input.is_action_pressed(keymap_name + "_left")
+    var RIGHT = Input.is_action_pressed(keymap_name + "_right")
+    var UP = Input.is_action_pressed(keymap_name + "_up")
+    var DOWN = Input.is_action_pressed(keymap_name + "_down")
+    
+    movedir.x = -int(LEFT) + int(RIGHT)
+    movedir.y = -int(UP) + int(DOWN)
+    
 func movement_loop():
-	var motion = movedir.normalized() * SPEED
-	move_and_slide(motion, Vector2(0,0))
+    var motion = movedir.normalized() * speed
+    move_and_slide(motion, Vector2(0,0))
