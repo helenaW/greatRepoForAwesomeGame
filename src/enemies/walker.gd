cc774e0176667c0f02b7b7a204328ff41a2f8f30
Helena Winkler <he_wi1@uni-bremen.de>
2019-02-27 11:09:51 +0100

    zombie walker added

---
+++ src/enemies/walker.gd
@@ -0,0 +1,33 @@
+extends "res://engine/entity.gd"
+
+export (int) var damage = 1
+
+var movetimer_length = 30
+var movetimer = 0
+
+func _ready():
+    $anim.play("default")
+    movedir = dir.rand()
+    
+func _physics_process(delta):
+    movement_loop()
+    damage_loop()
+    
+    if movetimer > 0:
+        movetimer -= 1
+    if movetimer == 0 || is_on_wall():
+        movedir = dir.rand()
+        movetimer = movetimer_length
+
+func damage_loop():
+    .damage_loop()
+    
+    if health <= 0:
+        var death_animation = preload("res://enemies/enemy_death.tscn").instance()
+        get_parent().add_child(death_animation)
+        death_animation.global_transform = global_transform
+        queue_free()
+
+func _on_hitbox_area_entered(area):
+    if area.get_parent().is_in_group('player'):
+        area.get_parent().damage(damage, global_transform.origin)
